a6dfa2651b2c4c9e2d6471b1873b390a55b6243a
zakamka <na@bulygin.su>
2023-12-10 00:36:47 +0300

    2

--- app.js
+++ app.js
@@ -2,25 +2,44 @@ let items = ""
 
 new Vue({
     el: '#app',
-    data() {
-        return { 
-            items: []
-         }
+    data: {
+        items: [],
+        newtodoitem: '',
+        sortedbyASC: true
+    },
+    created() {
+        this.fetch_data()
     },
     methods: {
-        // addProduct() {
-        //     if (this.newProduct.trim() !== '') {
-        //         this.products.push(this.newProduct)
-        //         this.newProduct = ''
-        //     }
-        // },
-        // removeProduct(index) {
-        //     this.products.splice(index, 1)
-        // },
+        addNewTodoItem: function () {
+            this.items.push({
+                id: this.item_maxId(),
+                title: this.newtodoitem,
+                userId: 6815,
+                completed: false
+            })
+            console.log(this.items[this.items.length - 1].id + " " + this.items[this.items.length-1].title)
+            this.newTodoText = ''
+        },
+        RemoveTodoItem(item) {
+            console.log("remove " + item.id)
+            this.items.splice(item, 1)
+        },
 
-        zhopa: function(){
+        Sort() {
+            sortBy = 'title'
+            console.log('sort')
+            if (this.sortedbyASC) {
+                this.items.sort((x, y) => (x[sortBy] > y[sortBy] ? -1 : 1));
+                this.sortedbyASC = false;
+            } else {
+                this.items.sort((x, y) => (x[sortBy] < y[sortBy] ? -1 : 1));
+                this.sortedbyASC = true;
+            }
+        },
+        fetch_data: function(){
             fetch('https://jsonplaceholder.typicode.com/todos/')
-            .then(response => response.json())
+            .then(result => result.json())
             .then(data => (this.items = data))
         },
 
@@ -32,6 +51,12 @@ new Vue({
         items_undone: function()
         {
             return this.items.filter(item => item.completed == false)
+        },
+
+        item_maxId: function () {
+            var m = Math.max(...this.items.map(o => o.id)) + 1
+            console.log(" - " + m)
+            return m
         }
 
     },
